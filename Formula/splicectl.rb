64f414a460146788d3f59458e93a17af6640cb1d
Christopher Maahs <cmaahs@splicemachine.com>
2020-05-18 09:33:03 -0500

    initial: Initial Checking for testing

---
+++ Formula/splicectl.rb
@@ -0,0 +1,41 @@
+class Splicectl < Formula
+  desc "This is the control CLI for Splice Machine databases on Kubernetes"
+  homepage "https://github.com/splicemaahs/homebrew-utility"
+  url "https://github.com/splicemaahs/homebrew-utility.git"
+  version "0.0.1"
+
+  def install
+    if Hardware::CPU.is_32_bit?
+      if OS.mac?
+        def install
+          bin.install "bin/darwin/386/splicectl"
+        end
+      elsif OS.linux?
+        def install
+          bin.install "bin/linux/386/splicectl"
+        end
+      end
+    else
+      if OS.mac?
+        def install
+          bin.install "bin/darwin/amd64/splicectl"
+        end
+      elsif OS.linux?
+        def install
+          bin.install "bin/linux/amd64/splicectl"
+        end
+      end
+    end
+  end
+
+  def caveats; <<~EOS
+    Currently the use of splicectl REQUIRES that KUBECONFIG environment 
+    variable be exported.
+
+    export KUBECONFIG=~/.kube/config
+
+    auto-selecting ~/.kube/config will be fixed in an upcoming update.
+
+  EOS
+  end
+end
---
+++ README.md
@@ -0,0 +1,14 @@
+# Splice Machine Utility 
+
+Collection of brew installable administrative scripts for managing Splice Machine
+Databases under Kubernetes
+
+## Utility Tools 
+
+### Formula splicectl
+
+`splicectl` is a tool for managing Splice Machine Database clusters under Kubernetes
+
+- bin/{os}/{arch}/splicectl
+
+

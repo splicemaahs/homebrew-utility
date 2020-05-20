15f7fbb91eae8e20c45da09e6719016af4a02e95
cloudspliceci <build@splicemachine.com>
2020-05-20 15:56:06 +0000

    publish splicectl cli to 0.0.10

--- Formula/splicectl.rb
+++ Formula/splicectl.rb
@@ -3,7 +3,7 @@ class Splicectl < Formula
   homepage "https://github.com/splicemaahs/homebrew-utility"
   url "https://github.com/splicemaahs/homebrew-utility.git"
   version "0.0.10"
-  revision 3
+  revision 4
 
   if Hardware::CPU.is_32_bit?
     if OS.mac?
